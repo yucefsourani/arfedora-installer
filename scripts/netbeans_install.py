ecba50be537721389f101960824a80b8a42ae04b
yucefsourani <youssef.m.sourani@gmail.com>
2017-05-30 17:57:49 +0300

    Add netbeans

---
+++ scripts/netbeans_install.py
@@ -0,0 +1,49 @@
+import subprocess
+import os
+import sys
+
+home = os.environ["HOME"]
+
+b = sys.argv[1]
+web = "http://download.netbeans.org/netbeans/8.2/final/bundles/netbeans-8.2-linux.sh"
+name = "/tmp/netbeans-8.2-linux.sh"
+location = "{}/netbeans-8.2".format(home)
+
+def install_():
+    if not os.path.isfile(name):
+        check = subprocess.call("curl -o {}  {}".format(name,web),shell=True)
+        if check != 0 :
+            exit(1)
+
+    check = subprocess.call("chmod 755 {}".format(name),shell=True)
+    if check != 0 :
+        exit(1)
+
+    check = subprocess.call("{} --silent \"-J-Dnb-base.installation.location={}\" \"-J-Dnb-base.jdk.location=/usr/lib/jvm/java-openjdk\" ".format(name,location),shell=True)
+    if check != 0 :
+        exit(1)
+    exit(0)
+
+
+def remove_():
+    check = subprocess.call("{}/uninstall.sh --silent".format(location),shell=True)
+    if check != 0 :
+        exit(1)
+    exit(0)
+
+def check_():
+    if os.path.isdir("{}/bin".format(location)):
+        exit(0)
+    else:
+        exit(1)
+
+
+
+if b=="check":
+    check_()
+
+elif b== "install":
+    install_()
+
+elif b== "remove":
+    remove_()
